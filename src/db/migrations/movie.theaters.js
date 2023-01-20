98271de8fe060fd4a73ef92ed24ab9ae929f9816
ravneet998 <109993408+ravneet998@users.noreply.github.com>
2023-01-20 00:07:53 -0500

    Update movie.theaters.js

--- src/db/migrations/movie.theaters.js
+++ src/db/migrations/movie.theaters.js
@@ -5,17 +5,17 @@ exports.up = function (knex) {
       .foreign("movie_id")
       .references("movie_id")
       .inTable("movies")
-      .onDelete("cascade");
+      .onDelete("CASCADE");
     table.integer("theater_id").unsigned().notNullable();
     table
       .foreign("theater_id")
       .references("theater_id")
       .inTable("theaters")
-      .onDelete("cascade");
+      .onDelete("CASCADE");
     table.boolean("is_showing");
+    table.timestamps(true, true);
   });
 };
-
 
 exports.down = function (knex) {
   return knex.schema.dropTable("movies_theaters");
