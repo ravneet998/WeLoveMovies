b0bef4382d0ce8cb21e7d2f9dbae890e653ac8c4
ravneet998 <109993408+ravneet998@users.noreply.github.com>
2023-01-19 22:28:08 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,26 +1,41 @@
-if (process.env.USER) require("dotenv").config();
+if (process.env.DATABASE_URL) require("dotenv").config();
+
 const express = require("express");
 const app = express();
-const cors = require("cors");
+
 const moviesRouter = require("./movies/movies.router");
-const theatersRouter = require("./theaters/theaters.router");
 const reviewsRouter = require("./reviews/reviews.router");
+const theatersRouter = require("./theaters/theaters.router");
 
-app.use(cors()); //enable cors for entire api
+const cors = require("cors");
+
+app.use(cors());
 app.use(express.json());
-app.use("/movies", moviesRouter);
-app.use("/theaters", theatersRouter);
-app.use("/reviews", reviewsRouter);
 
-// Not found handler
+
+
+
+const router = express.Router()
+router.get('/', cors(), (req, res) => {
+  res.json({ message:
+  'Welcome! You can access the data using these routes: /movies, /reviews, /theaters, /reviews/:reviewId, /movies/:movieId, /movies/:movieId/theaters, and /movies/:movieId/reviews.'});
+})
+
+app.use('/', router);
+
+
+
+
+app.use("/movies", moviesRouter);
+app.use("/reviews", reviewsRouter);
+app.use("/theaters", theatersRouter);
+ 
 app.use((req, res, next) => {
-  next({ status: 404, message: `Not found: ${req.originalUrl}` });
+  next({ status: 404, message: "That page doesn't exist." });
 });
 
-// Error handler
-app.use((error, req, res, next) => {
-  console.error(error);
-  const { status = 500, message = "Something went wrong!" } = error;
+app.use((err, req, res, next) => {
+  const { status = 500, message = "Something went wrong on our end!" } = err;
   res.status(status).json({ error: message });
 });
 
