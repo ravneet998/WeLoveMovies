5107380ef41d341437caefe562be635a2a47c4a5
ravneet998 <109993408+ravneet998@users.noreply.github.com>
2023-01-19 21:42:42 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,26 +1,24 @@
 if (process.env.USER) require("dotenv").config();
 const express = require("express");
-const app = express();
 const cors = require("cors");
-const moviesRouter = require("./movies/movies.router");
-const theatersRouter = require("./theaters/theaters.router");
-const reviewsRouter = require("./reviews/reviews.router");
+
+const notFound = require('./error/notFound');
+const errorHandler = require("./error/errorHandler");
+const theatersRouter = require('./theaters/theaters.router');
+const moviesRouter = require('./movies/movies.router');
+const reviewsRouter = require('./reviews/reviews.router');
+
+
+const app = express();
 
 app.use(cors());
 app.use(express.json());
+
 app.use("/movies", moviesRouter);
 app.use("/theaters", theatersRouter);
 app.use("/reviews", reviewsRouter);
 
-app.use((req, res, next) => {
-  next({ status: 404, message: `Not found: ${req.originalUrl}` });
-});
-
-app.use((error, req, res, next) => {
-  console.error(error);
-  const { status = 500, message = "Something went wrong!" } = error;
-  res.status(status).json({ error: message });
-});
-
+app.use(notFound);
+app.use(errorHandler);
 
 module.exports = app;
