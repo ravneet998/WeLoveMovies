f8a899de9182714c3d708bf725ae06489ac07837
ravneet998 <109993408+ravneet998@users.noreply.github.com>
2023-01-19 22:15:04 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,24 +1,27 @@
 if (process.env.USER) require("dotenv").config();
 const express = require("express");
-const cors = require("cors");
-
-const notFound = require('./error/notFound');
-const errorHandler = require("./error/errorHandler");
-const theatersRouter = require('./theaters/theaters.router');
-const moviesRouter = require('./movies/movies.router');
-const reviewsRouter = require('./reviews/reviews.router');
-
-
 const app = express();
+const cors = require("cors");
+const moviesRouter = require("./movies/movies.router");
+const theatersRouter = require("./theaters/theaters.router");
+const reviewsRouter = require("./reviews/reviews.router");
 
-app.use(cors());
+app.use(cors()); //enable cors for entire api
 app.use(express.json());
-
 app.use("/movies", moviesRouter);
 app.use("/theaters", theatersRouter);
 app.use("/reviews", reviewsRouter);
 
-app.use(notFound);
-app.use(errorHandler);
+// Not found handler
+app.use((req, res, next) => {
+  next({ status: 404, message: `Not found: ${req.originalUrl}` });
+});
+
+// Error handler
+app.use((error, req, res, next) => {
+  console.error(error);
+  const { status = 500, message = "Something went wrong!" } = error;
+  res.status(status).json({ error: message });
+});
 
 module.exports = app;
