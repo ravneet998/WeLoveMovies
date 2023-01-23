386c360180a0ce7c9b1c09d6c5253302a4655e94
ravneet998 <109993408+ravneet998@users.noreply.github.com>
2023-01-23 18:34:56 -0500

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -3,19 +3,20 @@ const path = require("path");
 require("dotenv").config();
 
 const {
-  NODE_ENV = "development",
+  //NODE_ENV = "development",
   DEVELOPMENT_DATABASE_URL,
   PRODUCTION_DATABASE_URL,
 } = process.env;
-const URL =
-  NODE_ENV === "production"
-    ? PRODUCTION_DATABASE_URL
-    : DEVELOPMENT_DATABASE_URL;
+
+//const URL =
+  //NODE_ENV === "production"
+   // ? PRODUCTION_DATABASE_URL
+    //: DEVELOPMENT_DATABASE_URL;
 
 module.exports = {
   development: {
     client: "postgresql",
-    connection: URL,
+    connection: DEVELOPMENT_DATABASE_URL,
     pool: { min: 0, max: 5 },
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
@@ -27,7 +28,8 @@ module.exports = {
 
   production: {
     client: "postgresql",
-    connection: URL,
+    connection: PRODUCTION_DATABASE_URL
+    ,
     pool: { min: 0, max: 5 },
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
