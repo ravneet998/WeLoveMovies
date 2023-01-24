1a27df7a0ac0337c59a327e06edc9745185b3639
ravneet998 <109993408+ravneet998@users.noreply.github.com>
2023-01-23 22:05:58 -0500

    Update movies.router.js

--- src/movies/movies.router.js
+++ src/movies/movies.router.js
@@ -1,4 +1,4 @@
-const router = require('express').Router({ mergeParams: true });
+/* const router = require('express').Router({ mergeParams: true });
 const controller = require('./movies.controller');
 const methodNotAllowed = require('../errors/methodNotAllowed');
 const readReviews = require('../reviews/reviews.controller');
@@ -23,5 +23,26 @@ router
     .get(controller.readReviews)
     .all(methodNotAllowed);
 
+
+module.exports = router; */
+
+const router = require("express").Router();
+const controller = require("./movies.controller");
+const theatersRouter = require("../theaters/theaters.router");
+const reviewsRouter = require("../reviews/reviews.router");
+const methodNotAllowed = require("../errors/methodNotAllowed");
+
+router.use("/:movieId/theaters", controller.validateMovieId, theatersRouter);
+router.use("/:movieId/reviews", controller.validateMovieId, reviewsRouter);
+
+router
+	.route("/")
+	.get(controller.list)
+	.all(methodNotAllowed);
+
+router
+	.route("/:movieId")
+	.get(controller.read)
+	.all(methodNotAllowed);
 
 module.exports = router;
