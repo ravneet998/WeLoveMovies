cc3637025e2aa829926d620f45d8e4ecbf31751e
ravneet998 <109993408+ravneet998@users.noreply.github.com>
2023-01-25 22:48:15 -0500

    Update movies.router.js

--- src/movies/movies.router.js
+++ src/movies/movies.router.js
@@ -1,28 +1,16 @@
-const router = require('express').Router({ mergeParams: true });
-const controller = require('./movies.controller');
-const methodNotAllowed = require('../errors/methodNotAllowed');
-const readReviews = require('../reviews/reviews.controller');
+const router = require("express").Router();
+const controller = require("./movies.controller");
+const methodNotAllowed = require("../errors/methodNotAllowed");
 
 router
-    .route('/')
-    .get(controller.list)
-    .all(methodNotAllowed);
-
+  .route("/:movieId/theaters")
+  .get(controller.listTheaters)
+  .all(methodNotAllowed);
 router
-    .route('/:movieId')
-    .get(controller.read)
-    .all(methodNotAllowed);
-
-router
-    .route('/:movieId/theaters')
-    .get(controller.readTheaters)
-    .all(methodNotAllowed);
-
-router
-    .route('/:movieId/reviews')
-    .get(controller.readReviews)
-    .all(methodNotAllowed);
-
+  .route("/:movieId/reviews")
+  .get(controller.listReviews)
+  .all(methodNotAllowed);
+router.route("/:movieId").get(controller.read).all(methodNotAllowed);
+router.route("/").get(controller.list).all(methodNotAllowed);
 
 module.exports = router;
-
